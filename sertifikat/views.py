eb8a3822c9ad4dda07c5ed7f690438cc656336ad
teguh <baruputra111@gmail.com>
2023-01-02 14:24:50 +0800

    buat file biar gak di save lasung ngirim filenya

--- sertifikat/views.py
+++ sertifikat/views.py
@@ -4,17 +4,23 @@ from .forms import Zipform
 from .pillow import process
 from .models import HslSertifikat
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, Http404
 
 @csrf_exempt
 def index(request):
     if request.method=='POST':
         form=Zipform(request.POST,request.FILES)
-        if form.is_valid():
-            filezip=process(request.FILES['image'],request.FILES['datacsv'],request.POST.get("position",""),request.POST.get("size",""),request.POST.get("colour",""),request.POST.get("font",""))
-            zipsave=HslSertifikat(file=filezip)
-            zipsave.save()
-            print(zipsave.file.url)
-    return render(request,'sertifikat/index.html',context={"down":zipsave})
+        print(request.POST)
+        print("request file")
+        print(request.FILES)
+        filezip=process(request.FILES['image'],request.FILES['datacsv'],request.POST.get("position",""),request.POST.get("size",""),request.POST.get("color",""),request.POST.get("font",""))
+        zipsave=HslSertifikat(file=filezip)
+        # zipsave.save()
+        response = HttpResponse(filezip.read(), content_type="application/zip")
+        response['Content-Disposition'] = 'inline; filename=' + str(filezip)
+        return response
+    raise Http404
+    # return render(request,'sertifikat/index.html',context={"down":zipsave})
 
 def test(request):
     return render(request,'sertifikat/test.html')
